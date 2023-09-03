fa94cd3a385971cb050bc6bbd41a04f862ba1b57
IvavIvntsk <ivan.ivanytskyii@gmail.com>
2023-09-03 23:55:02 +0300

    100

--- src/services/apiServices.ts
+++ src/services/apiServices.ts
@@ -1,8 +1,9 @@
 import axios, {AxiosError, AxiosResponse} from "axios";
-import {baseURL} from "../constants";
+import {baseURL, urls} from "../constants";
 import {authServices} from "./authServices";
 import {Simulate} from "react-dom/test-utils";
 import error = Simulate.error;
+import {router} from "../router";
 
 type IRes<DATA> = Promise<AxiosResponse<DATA>>
 
@@ -18,6 +19,8 @@ apiServices.interceptors.request.use(req=>{
     return req
 })
 let isRefreshing = false
+
+const waitList:IWaitList[] = []
 apiServices.interceptors.response.use(
     res =>{
         return res
@@ -30,17 +33,36 @@ apiServices.interceptors.response.use(
                 try {
                     await authServices.refresh()
                     isRefreshing = false
+                    afterRefresh()
                     return apiServices(originalRequest)
                 }catch (e){
                     authServices.deleteTokens()
                     isRefreshing = false
+                    router.navigate('/login?SessionExpired=true')
                     return Promise.reject(error)
                 }
             }
+        if (originalRequest.url === urls.auth.refresh){
+            return Promise.reject(error)
         }
+        return new Promise(resolve => {
+            subscribeToWaitList(() => resolve(apiServices(originalRequest)))
+        })
+        }
+
         return Promise.reject(error)
     }
 )
+type IWaitList = () => void
+const subscribeToWaitList =(cb:IWaitList):void => {
+    waitList.push(cb)
+}
+const afterRefresh = ():void => {
+    while (waitList.length) {
+        const cb = waitList.pop()
+        cb()
+    }
+}
 
 export {apiServices}
 export type {IRes}
